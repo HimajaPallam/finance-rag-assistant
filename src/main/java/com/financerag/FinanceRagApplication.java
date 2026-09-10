f8c9909bf8fc2455b0ba4b6812962434f7d045fd
unknown <pallamreddyhimaja24@gmail.com>
2026-09-10 22:16:44 +0530

    Initial commit

---
+++ src/main/java/com/financerag/FinanceRagApplication.java
@@ -0,0 +1,12 @@
+package com.financerag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceRagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceRagApplication.class, args);
+    }
+}
